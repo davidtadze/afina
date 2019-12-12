33d92dbf75340d811be4a6397592fc627881468c
davidtadze <davidtadze@gmail.com>
2019-12-12 18:14:47 +0300

    Add mutexes

--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -3,7 +3,12 @@
 
 #include <cstring>
 
+#include <mutex>
 #include <sys/epoll.h>
+#include <spdlog/logger.h>
+
+#include <afina/execute/Command.h>
+#include <protocol/Parser.h>
 
 namespace Afina {
 namespace Network {
@@ -11,12 +16,15 @@ namespace MTnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps, std::shared_ptr<spdlog::logger> pl) :
+            _socket(s),
+            _pStorage(std::move(ps)),
+            _logger(std::move(pl)) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
-    }
+        }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return _is_alive; }
 
     void Start();
 
@@ -27,11 +35,28 @@ protected:
     void DoWrite();
 
 private:
-    friend class Worker;
     friend class ServerImpl;
+    friend class Worker;
 
     int _socket;
     struct epoll_event _event;
+
+    bool _is_alive;
+    std::shared_ptr<spdlog::logger> _logger;
+
+    int _already_read_bytes = 0;
+    std::size_t _arg_remains;
+    Protocol::Parser _parser;
+    std::string _argument_for_command;
+    std::unique_ptr<Execute::Command> _command_to_execute;
+    char _client_buffer[4096];
+
+    std::shared_ptr<Afina::Storage> _pStorage;
+
+    std::vector<std::string> _answer_buf;
+    int _cur_position = 0;
+
+    std::mutex _mutex;
 };
 
 } // namespace MTnonblock
